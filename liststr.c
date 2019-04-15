97d571b311f49817cb77ce8ce614515cef6102ef
Jason Cortella <419@holbertonschool.com>
2019-04-15 18:59:40 +0000

    Split print_list into print_list and print_list_str

--- liststr.c
+++ liststr.c
@@ -74,12 +74,12 @@ list_t *add_node_end(list_t **head, const char *str, int num)
 }
 
 /**
- * print_list - prints a linked lists
+ * print_list_str - prints only the str element of a list_t linked list
  * @h: pointer to first node
  *
  * Return: size of list
  */
-size_t print_list(const list_t *h)
+size_t print_list_str(const list_t *h)
 {
 	size_t i = 0;
 
--- liststr.h
+++ liststr.h
@@ -16,12 +16,13 @@ typedef struct liststr
 /* liststr.c module */
 list_t *add_node(list_t **, const char *, int);
 list_t *add_node_end(list_t **, const char *, int);
-size_t print_list(const list_t *);
+size_t print_list_str(const list_t *);
 int delete_node_at_index(list_t **, unsigned int);
 void free_list(list_t **);
 
 /* liststr2.c module */
 size_t list_len(const list_t *);
 char **list_to_strings(list_t *);
+size_t print_list(const list_t *);
 
 #endif
