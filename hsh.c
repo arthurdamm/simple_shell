bd5f0cd539707d43edf68c36611d65978793ef98
Arthur Damm <indigoarthur@gmail.com>
2019-04-18 00:59:55 +0000

    Fix Permission Denied msg

--- hsh.c
+++ hsh.c
@@ -144,7 +144,8 @@ void fork_cmd(info_t *info)
 		if (execve(info->path, info->argv, get_environ(info)) == -1)
 		{
 			free_info(info, 1);
-			exit(1);
+			if (errno == EACCES)
+				exit(126);
 		}
 		/* TODO: PUT ERROR FUNCTION */
 	}
@@ -154,6 +155,8 @@ void fork_cmd(info_t *info)
 		if (WIFEXITED(info->status))
 		{
 			info->status = WEXITSTATUS(info->status);
+			if (info->status == 126)
+				print_error(info, "Permission denied\n");
 		}
 	}
 }
