7c7041fac8670dc309c9e0c2d5a45874d8e56764
Arthur Damm <indigoarthur@gmail.com>
2019-04-19 05:15:22 +0000

    Fix comments to only work after space

--- more_functions2.c
+++ more_functions2.c
@@ -92,7 +92,7 @@ void remove_comments(char *buf)
 	int i;
 
 	for (i = 0; buf[i] != '\0'; i++)
-		if (buf[i] == '#')
+		if (buf[i] == '#' && i && buf[i - 1] == ' ')
 		{
 			buf[i] = '\0';
 			break;
