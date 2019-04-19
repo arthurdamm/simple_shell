5451a7ae6954664136157b2cec15964337b5b19c
Arthur Damm <indigoarthur@gmail.com>
2019-04-19 04:50:47 +0000

    Add error message for file as input

--- main.c
+++ main.c
@@ -14,14 +14,22 @@ int main(int ac, char **av)
 
 	if (interactive(info) && (ac == 2))
 	{
+		info->arg = av[0];
+		set_info(info, av);
 		fd = open(av[1], O_RDONLY);
-
 		if (fd == -1)
 		{
 			if (errno == EACCES)
 				exit(126);
 			if (errno == ENOENT)
+			{
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
 				exit(127);
+			}
 			return (EXIT_FAILURE);
 		}
 		info->readfd = fd;
